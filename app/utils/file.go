53a23a1b161283b5f704c817b2b7bf891529056b
AlexisPin <pinceminalexis@gmail.com>
2025-02-17 17:28:01 +0100

    debug

--- app/utils/file.go
+++ app/utils/file.go
@@ -7,7 +7,9 @@ import (
 )
 
 func ReadFile(path string) (*bytes.Buffer, error) {
+	fmt.Printf("Reading file %s\n", path)
 	buffer, err := os.ReadFile(path)
+	fmt.Printf("Read file %s\n", path)
 	if err != nil {
 		return new(bytes.Buffer), fmt.Errorf("unable to read file %s: %w", path, err)
 	}
